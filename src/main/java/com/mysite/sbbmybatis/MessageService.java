b09dd5eb80dffc4bd43aa3333f0a97f275f1a1f9
cozyiscat <yuiop7664@gmail.com>
2024-10-07 11:41:49 +0900

    10/2 수업까지 여기

---
+++ src/main/java/com/mysite/sbbmybatis/MessageService.java
@@ -0,0 +1,36 @@
+package com.mysite.sbbmybatis;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+	@Autowired
+	private MessageMapper messageMapper;
+	
+	// 데이터베이스에서 값을 가져오는 것
+	public Message getMessage(Integer id) {
+		return messageMapper.getMessageById(id);
+		//return "Hello World";
+	}
+	
+	public void createMessage(Message message) {
+		messageMapper.insertMessage(message);
+	}
+	
+	public List<Message> getMessagesLimit10() {
+		return messageMapper.getMessagesLimit10();
+	}
+	
+	//페이지 계산
+//	public PageDTO getPagedData(int page, int size) {
+//        int offset = (page - 1) * size;
+//        List<YourDataType> content = mapper.selectWithPaging(offset, size);
+//        int totalElements = mapper.countTotal();
+//        int totalPages = (int) Math.ceil((double) totalElements / size);
+//
+//        return new PageDTO(page, size, totalPages, totalElements, content);
+//    }
+}
